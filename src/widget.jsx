778022fbf756aa93fcac756a452e325838c04ad0
SCunha24 <156427767+SCunha24@users.noreply.github.com>
2025-12-11 21:43:31 +0000

    feat(widgets): Add a widget class and style it

--- src/widget.jsx
+++ src/widget.jsx
@@ -3,7 +3,7 @@ const { useState, useEffect } = React
 function Widget({ w, h, color, content }) {
 
     return (
-        <div style={{ width: w, height: h, backgroundColor: color }}>
+        <div className="widget" style={{ width: w, height: h, backgroundColor: color }}>
             {content}
         </div>
     )
