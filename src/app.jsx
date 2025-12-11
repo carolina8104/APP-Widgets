57d460fface6c1dc19a740de2d40927733dc560d
SCunha24 <156427767+SCunha24@users.noreply.github.com>
2025-12-11 22:54:48 +0000

    feat(layout): Add a grid for all the widgets

--- src/app.jsx
+++ src/app.jsx
@@ -3,9 +3,37 @@ const { useState, useEffect } = React
 function App() {
 
     return (
-      <div className="notes">
-        <Widget color="var(--accent-yellow)" content={<Notes userId="user123" />} />
-      </div>
+        <div className="dashboard-grid">
+
+            <div className="friends">
+                <Widget color="var(--accent-neon)" content={""} />
+            </div>
+
+            <div className="timeTracker">
+                <Widget color="var(--accent-neon)" content={""} />
+            </div>
+
+             <div className="notes">
+                <Widget color="var(--accent-yellow)" content={<Notes userId="user123" />} />
+            </div>
+
+            <div className="tasks">
+                <Widget color="var(--accent-neon)" content={""} />
+            </div>
+
+            <div className="photo">
+                <Widget color="var(--accent-neon)" content={""} />
+            </div>
+
+            <div className="progress">
+                <Widget color="var(--accent-neon)" content={""} />
+            </div>
+
+            <div className="calendar">
+                <Widget color="var(--accent-neon)" content={""} />
+            </div>
+
+        </div>
     )
 }
 
--- src/widget.jsx
+++ src/widget.jsx
@@ -1,11 +1,11 @@
-const { useState, useEffect } = React
-
-function Widget({ w, h, color, content }) {
+function Widget(props) { 
+    
+    const {color, content } = props 
 
     return (
-        <div className="widget" style={{ width: w, height: h, backgroundColor: color }}>
+        <div className="widget" style={{ backgroundColor: color }}>
             {content}
         </div>
     )
- 
 }
+
